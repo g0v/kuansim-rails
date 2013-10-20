e92d2d4e93492d10648b77abe6572045023908e4
saasbook <saasbook@saasbook.(none)>
2013-10-19 23:19:15 -0700

    First commit!

---
+++ kuansim/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuansim::Application.config.secret_token = '7eda75e645b846d5baf3368341b79b5468529b44cf8ef2fe4952259a1511772c9262c3546a5003003b841ab97cec55d6a448d2574fe83fccc2a79c8d682e9114'
