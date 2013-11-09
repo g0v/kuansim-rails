dd2c214a67ea6319d1523f45fdf8fb90077a4e59
Jerry Chung <jerry.chung92@gmail.com>
2013-11-09 15:51:16 -0800

    json backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Kuansim::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resources :events
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  root :to => "home#index"
 
-  match 'users/login' => 'users#login', :as => :user_login
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -51,10 +49,21 @@ Kuansim::Application.routes.draw do
   #     resources :products
   #   end
 
+  # namespace :api, defaults: {format: 'json'} do
+  #   resources :users
+  # end
+
+  namespace :api, defaults: {format: :json} do
+    
+  end
+
+  post '/users/authenticate', to: 'users#authenticate'
+
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect('/index.html')
-  root :to => 'static#show'
+  # root :to => redirect('/index.html')
+  # root :to => 'static#show'
 
   # See how all your routes lay out with "rake routes"
 
