ebf6c778c630f97b89f66e88debd72ad7ae3eb41
Andrew Chen <aychen92@gmail.com>
2013-11-09 16:19:30 -0800

    Added new migration to change event datetime from string back to datetime.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,7 +5,7 @@ describe EventsController do
 	describe 'create' do
 		it 'should create a new event' do
 			fake_data = {"title" => 'Bart Strike',
-						 "datetime" => '10/18/2013',
+						 "datetime" => DateTime.new(2013, 10, 18),
 						 "location" => 'San Francisco, CA',
 						 "description" => "This is a horrible event!"}
 			Event.should_receive(:create).with(fake_data)
