429fb35d59469f2adc0b554c70993e0d0152fba6
Andrew Chen <aychen92@gmail.com>
2013-11-10 13:59:31 -0800

    "Fixed bug in Event.rb"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :datetime, :description, :location, :title, :issue_id
   has_many :tags
-  belongs_to :user, :issue
+  belongs_to :user
+  belongs_to :issue
 end
