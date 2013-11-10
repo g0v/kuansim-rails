c62ea3c7f1a9fc1929706a0b4297659c67021792
Andrew Chen <aychen92@gmail.com>
2013-11-10 14:06:20 -0800

    Changed an attr_accessor var name in Event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :datetime, :description, :location, :title, :issue_id
+  attr_accessible :date_happened, :description, :location, :title, :issue_id
   has_many :tags
   belongs_to :user
   belongs_to :issue
