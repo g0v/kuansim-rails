82198bfdb768962effa9018956e3343d09d191f4
Jerry Chung <jerry.chung92@gmail.com>
2013-11-10 22:26:13 -0800

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(:version => 20131110103619) do
     t.text     "description"
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
-    t.integer  "user_id"
     t.integer  "issue_id"
+    t.integer  "user_id"
   end
 
   create_table "issues", :force => true do |t|
