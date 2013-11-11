0491ff0418af9e5d93edd5c30ff877c37f6eba9a
Michael Huang <chmhuang.tw@gmail.com>
2013-11-10 17:01:33 -0800

    Added some spec tests for models

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	before :each do 
+		@event = Event.new(datetime: "May 14, 2014", description: "Some words", location: "San Jose, CA", title: "Fake title", issue_id: "123")
+	end
+	it "should have a datetime, description, title" do
+		@event.should be_an_instance_of Event
+	end
+	it "should have a valid description" do
+		@event.description.should eq"Some words"
+	end
+	it "should have a valid title" do 
+		@event.title.should eq "Fake title"
+	end
 end
