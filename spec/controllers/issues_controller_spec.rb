7a5e5c9ee886d983fa67acf5fb0a01fc048757ee
Rae Liu <happyaray@gmail.com>
2013-11-10 23:17:38 -0800

    add spec for listing issues

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -16,4 +16,35 @@ describe IssuesController do
     response.body.should have_content("#InsufficientAlcohol")
   end
 
+  describe 'listing all issues' do
+    before :each do
+      first = mock(Issue)
+      first.stub(:id).and_return('1')
+      first.stub(:title).and_return('first issue title')
+      first.stub(:description).and_return('issue description')
+
+      second = mock(Issue)
+      second.stub(:id).and_return('2')
+      second.stub(:title).and_return('second issue title')
+      second.stub(:description).and_return('issue description2')
+
+      issues = [first, second]
+
+      Issue.stub(:find).and_return(issues)
+    end
+
+
+    it 'lists all the issues' do
+      get :list_all_issues
+      response.body.should have_content("1")
+      response.body.should have_content("2")
+      response.body.should have_content("first issue title")
+      response.body.should have_content("second issue title")
+      response.body.should have_content("issue description2")
+    end
+
+
+
+  end
+
 end
