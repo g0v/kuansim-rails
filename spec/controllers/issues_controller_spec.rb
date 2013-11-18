99b47349e7f37991fba1ccd5fd3fb29386c29c63
David Seeto <seeto.david@gmail.com>
2013-11-17 18:41:29 -0800

    Updated simple CRUD rspec tests for Issues.

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -1,19 +1,79 @@
 require 'spec_helper'
 
 describe IssuesController do
+    describe 'create' do
+    it 'should create a new issue' do
+      fake_data = {"issue" => {
+                    "title" => 'Bart Strike',
+                    "datetime" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+                    "description" => "Renegotiating employee contract."}}
+      fake_mod_data = {"title" => 'Bart Strike',
+                       "datetime" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+                       "description" => "Renegotiating employee contract."}
+      Issue.should_receive(:create).with(fake_mod_data)
+      post :create, fake_data
+    end
 
-  before :each do
+    it 'should not create a new issue if no params are supplied' do
+      Issue.should_not_receive(:create)
+      post :create
+    end
   end
 
-  it "should successfully return data json for given issue" do
-    load "#{Rails.root}/db/seeds.rb"
-    id = Issue.find_by_title("Issue 0")
-    get :timeline, issue_id: id
-    response.body.should have_content("Issue 0")
-    response.body.should have_content("Jerry's drinking problem.")
-    response.body.should have_content("Group Meeting Binge Drinking")
-    response.body.should have_content("5 Beers!")
-    response.body.should have_content("#InsufficientAlcohol")
+  describe 'delete' do
+    it 'should delete the selected issue' do
+      issue = FactoryGirl.create(:issue)
+      Issue.stub(:find) {issue}
+      Issue.should_receive(:find).with(issue.id)
+      Issue.should_receive(:delete).with(issue.id)
+      delete :delete, {:id => issue.id}
+    end
+  end
+
+  describe 'update' do
+    it 'should update the selected issue' do
+      issue = FactoryGirl.create(:issue)
+      fake_data = {"issue" => {
+              "title" => 'Bart Strike',
+              "datetime" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+              "description" => "Renegotiating employee contract."}}
+      fake_mod_data = {"title" => 'Bart Strike',
+                       "datetime" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+                       "description" => "Renegotiating employee contract."}
+      Issue.stub(:find) {issue}
+      Issue.should_receive(:find).with(issue.id)
+      issue.should_receive(:update_attributes).with(fake_mod_data)
+      put :update, fake_data
+      puts response.body
+    end
+
+    it 'should not update a new issue if the issue does not exist' do
+      event = FactoryGirl.create(:issue)
+      Issie.stub(:find) {issue}
+      issue.should_not_receive(:update_attributes)
+      fake_data = { "id" => 5,
+                    {"issue" => {
+              "title" => 'Bart Strike',
+              "datetime" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+              "description" => "Renegotiating employee contract."}}
+      put :update, fake_data
+    end
+  end
+
+  decribe 'timeline' do
+    before :each do
+    end
+
+    it "should successfully return data json for given issue" do
+      load "#{Rails.root}/db/seeds.rb"
+      id = Issue.find_by_title("Issue 0")
+      get :timeline, issue_id: id
+      response.body.should have_content("Issue 0")
+      response.body.should have_content("Jerry's drinking problem.")
+      response.body.should have_content("Group Meeting Binge Drinking")
+      response.body.should have_content("5 Beers!")
+      response.body.should have_content("#InsufficientAlcohol")
+    end
   end
 
   describe 'listing all issues' do
