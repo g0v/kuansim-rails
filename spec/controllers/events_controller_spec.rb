67094206db307f8621d7aab24f4fcaed3e8ac1d8
Rae Liu <happyaray@gmail.com>
2013-11-25 02:23:28 -0800

    add more test to bring up the test coverage

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,6 +4,27 @@ require 'json'
 
 describe EventsController do
   describe 'create' do
+    it 'creates a bookmark from bookmarklet request' do
+      @user_goog = FactoryGirl.create(:user_google)
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      fake_data = {"event" => {
+                    "title" => 'Bart Strike',
+                    "location" => 'San Francisco, CA',
+                    "description" => "This is a horrible event!",
+                    "date_happened" => '1234567'},
+                   'current_bookmark_user' => '222333'}
+      fake_mod_data = {"title" => 'Bart Strike',
+                       "location" => 'San Francisco, CA',
+                       "description" => "This is a horrible event!",
+                       "date_happened" => DateTime.parse(Time.at(1234567.0 / 1000.0).to_s),
+                       "user_id" => @user_goog.id}
+      Event.should_receive(:create).with(fake_mod_data)
+      cookies.stub(:signed).and_return({:user_c => 300})
+      User.stub(:find).and_return(@user_goog)
+      post :create, fake_data
+      response.should be_success
+    end
+
     it 'should create a new event' do
       user = FactoryGirl.create(:user)
       sign_in user
@@ -107,6 +128,38 @@ describe EventsController do
       Event.should_receive(:take)
       Event.should_not_receive(:all)
       get :get_events, {:limit => 5}
+    end
+  end
+
+  describe "Bookmarklet view render" do
+    before :each do
+      @user_goog = FactoryGirl.create(:user_google)
+      @user_face = FactoryGirl.create(:user_facebook)
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+    it "renders the bookmarklet view without login" do
+      get :add_to_bookmark_btn
+      response.should render_template("add_to_bookmark_btn")
+    end
+    it "renders the bookmarklet view because cannot find user" do
+      request.cookies[:user_c] = '12345'
+      cookies.stub(:signed).and_return({:user_c => 300})
+      User.stub(:find).and_return(nil)
+      get :add_to_bookmark_btn
+      response.should render_template("add_to_bookmark_btn")
+    end
+    it "renders the bookmarklet view after login with match" do
+      User.stub(:find).and_return(@user_goog)
+      request.cookies[:user_c] = '12345'
+      cookies.stub(:signed).and_return({:user_c => @user_goog.id})
+      get :add_to_bookmark_btn
+      response.should render_template("add_to_bookmark_btn")
+    end
+    it "renders the bookmarklet view if cookie is set" do
+      sign_in @user_goog
+      cookies.stub(:signed).and_return({:user_c => @user_goog.id})
+      get :add_to_bookmark_btn
+      response.should render_template("add_to_bookmark_btn")
     end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe Event do
-  	before :each do 
-		@event = Event.new(date_happened: "112233", description: "Some words", location: "San Jose, CA", title: "Fake title", issue_id: "123")
-	end
-	it "should have a datetime, description, title" do
-		@event.should be_an_instance_of Event
-	end
-	it "should have a valid description" do
-		@event.description.should eq"Some words"
-	end
-	it "should have a valid title" do 
-		@event.title.should eq "Fake title"
-	end
-	it "should be able to be as json" do
-		h = {"id" => "1", "title" => "ASDF", "date_happened" => "112233"} 
-		expect{@event.as_json(h)}.to be_true
+	describe "Event CRUD actions" do
+  	before :each do
+			@event = Event.new(date_happened: "112233", description: "Some words", location: "San Jose, CA", title: "Fake title", issue_id: "123")
+		end
+		it "should have a datetime, description, title" do
+			@event.should be_an_instance_of Event
+		end
+		it "should have a valid description" do
+			@event.description.should eq"Some words"
+		end
+		it "should have a valid title" do
+			@event.title.should eq "Fake title"
+		end
+		it "should be able to be as json" do
+			h = {"id" => "1", "title" => "ASDF", "date_happened" => "112233"}
+			expect{@event.as_json(h)}.to be_true
+		end
 	end
 end
