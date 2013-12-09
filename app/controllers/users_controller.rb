170073db721d5762c45fdd84c6796ace880d9772
Jason.S.Chen <jasonschen5@gmail.com>
2013-12-08 16:28:12 -0800

    added method to get events created by given user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -109,15 +109,11 @@ class UsersController < ApplicationController
 
   def created_events
     json_reply = {success: true}
-    events = []
-    u_id = params[:id]
-    i_id = params[:issue_id]
-    if !is_int? u_id or !is_int? i_id
-      json_reply[:success] = false
-      json_reply[:message] = "A user_id and issue_id must be provided."
+    events = User.find_by_id(params[:id]);
+    if (events.nil?)
+      events = []
     else
-      events = User.find(u_id).events.select{ |e| e.issues.include?(Issue.find(i_id)) }
-      json_reply[:message] = "Events of the given user that are associated with the given issue were returned."
+      events = events.map{ |e| e.as_json[:og] = e.og_tags }
     end
     json_reply[:events] = events
     render json: json_reply
